ee054cd8b43b5e5b2f64314dd54089a7efa14826
Gavin Tranter <gavintranter@users.noreply.github.com>
2020-06-23 12:38:23 +0100

    Simplify processing commands

--- src/main/kotlin/uk/trantr/kata/marsroverk/Rover.kt
+++ src/main/kotlin/uk/trantr/kata/marsroverk/Rover.kt
@@ -1,19 +1,11 @@
 package uk.trantr.kata.marsroverk
 
-import uk.trantr.kata.marsroverk.Command.*
 import uk.trantr.kata.marsroverk.navigation.Position
 
 class Rover(val position: Position) {
     fun receive(commands: Array<Char>): Rover {
         val p = commands.map { Command.from(it) }
-            .fold(this.position) { deltaP, next ->
-                when (next) {
-                    RIGHT -> next.execute(deltaP)
-                    LEFT -> next.execute(deltaP)
-                    FORWARD -> next.execute(deltaP)
-                    BACKWARD -> next.execute(deltaP)
-                }
-            }
+            .fold(this.position) { deltaP, next -> next.execute(deltaP) }
 
         return Rover(p)
     }
